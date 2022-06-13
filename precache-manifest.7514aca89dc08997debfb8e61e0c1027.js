642903521d30adfa6fe79c2fe8e5dd7cc390f41b
thepetruha <petrkrivosekov@gmail.com>
2022-06-14 02:47:09 +0300

    Updates

---
+++ precache-manifest.7514aca89dc08997debfb8e61e0c1027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86ff3fae68f884bc25a8",
+    "url": "/landing-safi.github.io/static/css/main.f4eb8958.chunk.css"
+  },
+  {
+    "revision": "86ff3fae68f884bc25a8",
+    "url": "/landing-safi.github.io/static/js/main.86ff3fae.chunk.js"
+  },
+  {
+    "revision": "f993ed0ea40465f97e7e",
+    "url": "/landing-safi.github.io/static/js/1.f993ed0e.chunk.js"
+  },
+  {
+    "revision": "9dff431a885bae8fc337",
+    "url": "/landing-safi.github.io/static/js/2.9dff431a.chunk.js"
+  },
+  {
+    "revision": "3a060ab85e00c6df16ed",
+    "url": "/landing-safi.github.io/static/js/runtime~main.3a060ab8.js"
+  },
+  {
+    "revision": "0f9b27442e439d56678a8e41ba6fb91b",
+    "url": "/landing-safi.github.io/index.html"
+  }
+];
